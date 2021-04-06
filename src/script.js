80d7f164ddebdf6dda505c1d05e2a2b43270536d
Charlotte Li <li.zihao@columbia.edu>
2021-04-05 23:55:25 -0400

    word tree init

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import textBalancer from 'text-balancer';
 import { intersectTop } from './scripts/utils';
 import './scripts/chord-chart';
+import './scripts/word-tree';
 
 import { spectate as spectateConfig } from '../package.json';
