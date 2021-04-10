265fbf09ceff4049122c6b44c12072e7bba3d0cc
Charlotte Li <li.zihao@columbia.edu>
2021-04-10 00:44:52 -0400

    Deploy to gh-pages

--- src/script.js
+++
@@ -1,7 +0,0 @@
-import textBalancer from 'text-balancer';
-import { intersectTop } from './scripts/utils';
-import './scripts/chord-chart';
-import './scripts/word-tree';
-import './scripts/arc';
-
-import { spectate as spectateConfig } from '../package.json';
